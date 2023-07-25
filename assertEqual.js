c6e8e2a88d93895f1796efb2d61bb75b032ea4f0
ralph <ralphdcouto1987@gmail.com>
2023-07-24 21:18:08 -0400

    Created a function for Assert

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function (actual, expected){
+  if (actual === expected) return "Assertion Passed";
+  if (actual !== expected) return "Assertion Failed";
+   
+};
+
+console.log(assertEqual(1, 1));
