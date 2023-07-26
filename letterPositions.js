b954711a16a2be955dce47111172c334751eb438
ralph <ralphdcouto1987@gmail.com>
2023-07-26 18:38:10 -0400

    return all the indices (zero-based positions) in the string where each character is found.

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+let word = "Light house labs"
+
+const letterPositions = function(sentence) {
+  const results = [];
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i]
+    if (letter === ' ') {
+      continue;
+    } else if (!results[letter]) {
+      results[letter] = [i]
+    } else {
+      results[letter].push(i)
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions(word));
+
