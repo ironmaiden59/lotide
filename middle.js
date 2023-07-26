2da8f003d2c9e2b6557bfcb54eefc03d8ffccf77
ralph <ralphdcouto1987@gmail.com>
2023-07-26 10:03:44 -0400

    Implement middle which will take in an array and return the middle-most element(s) of the given array.

---
+++ middle.js
@@ -0,0 +1,46 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // console.log("Arrays not equal length")
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      // if it is an array && the arrays are not equal then return false;
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      // if the element is not an array and the items are not equal then return false
+      return false;
+    }
+  } 
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
+
+    const middle = function(array) {
+      if (array.length < 3) {
+        return [];
+      } else if (array.length % 2 === 0) {
+        return array.slice(array.length / 2 - 1, array.length / 2 + 1)
+      } else {
+         let newArray = [];
+         let middleNum = Math.round(array.length /2)
+         newArray.push(array[middleNum -1]);
+         return newArray;
+      }
+    };
+
+    console.log(middle([1]));
+    console.log(middle([1, 2]));
+
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
