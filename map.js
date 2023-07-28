2463a8d5174f7ba66599e19b80cd7b0f958341e7
ralph <ralphdcouto1987@gmail.com>
2023-07-28 00:05:12 -0400

    Define an empty map function

---
+++ map.js
@@ -0,0 +1,12 @@
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function() {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results;
+}
+
