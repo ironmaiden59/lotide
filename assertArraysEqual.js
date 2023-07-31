888ce53cb21b3c4d5bab1b7bbaa101fde71d3fe0
ralph <ralphdcouto1987@gmail.com>
2023-07-31 19:01:26 -0400

    Updated the file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    // console.log("Arrays not equal length")
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-      // if it is an array && the arrays are not equal then return false;
-      return false;
-    }
-    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-      // if the element is not an array and the items are not equal then return false
-      return false;
-    }
-  } 
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -24,3 +8,6 @@ const assertArraysEqual = function(actual, expected) {
       }
     };
 
+    module.exports = assertArraysEqual;
+
+console.log(assertArraysEqual([1, 2, 3]) ([1, 2, 3]));
