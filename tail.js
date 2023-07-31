a9a93e775d52044d0ddf17de9688d7616608e454
ralph <ralphdcouto1987@gmail.com>
2023-07-31 16:28:14 -0400

    updated tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,12 @@
-const assertEqual = function (actual, expected){
-  if (actual === expected) return "Assertion Passed";
-  if (actual !== expected) return "Assertion Failed";
-   
-};
+
 
 function tail(array) {
-  if (Array.isArray(array) && array.length > 1) {
-    return array.slice(1);
+  if (Array.isArray(array) && array.length > 0) {
+    return array.slice(-1);
   } else {
     return []; // Return an empty array if the input array is empty or has only one element
   }
 }
 
-
+module.exports = tail
 
